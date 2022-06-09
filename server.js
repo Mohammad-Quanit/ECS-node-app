f69b1f8e53e4f6c251bfd06f52ae3e7f9ad5e0f8
Mohammad-Quanit <muhammadquanit@gmail.com>
2022-06-09 22:44:35 +0500

    changed: buildspec.yml & fixed server code

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express')
+const router = express.Router();
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 const HOST = '0.0.0.0';
 
-app.get('/healthcheck', (req, res) => res.status(200).json({status: 'ok'}));
-app.get('/version', (req, res) => res.status(200).json({status: 'Application Version 1'}));
+router.get('/', (req, res) => res.status(200).json({status: 'Application Version 1'}));
 
+app.use('/api', router);
 app.listen(PORT, HOST);
-
 console.log(`Running on http://${HOST}:${PORT}`);
