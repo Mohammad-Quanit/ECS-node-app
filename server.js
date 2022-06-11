ca3003183bb1fcf0de384711144c7ff1d25fcb76
Mohammad-Quanit <muhammadquanit@gmail.com>
2022-06-11 23:48:54 +0500

    Adding build specification

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
-app.use('/', (req, res) => res.status(200).json({status: 'Application Version 2.0'}));
+app.use('/', (req, res) => res.status(200).json({status: 'Application Version 1.0'}));
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
